600d42edf1219528b837c9eed225abfa9c2d8432
Green-Zero-Development <admin@greenzero.us>
2019-10-14 18:06:22 -0400

    add form to footer of homepage

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -37,7 +37,11 @@ footer .widget p {
     <a href="tel:9122250938" class="w-full font-oswald text-2xl md:text-3xl text-white font-bold text-center uppercase py-8 md:py-2">Call Us - (912) 225-0938</a>
   </div>
   <div class="sm:flex flex-wrap text-white px-10">
-    <div class="sm:flex flex-wrap w-full py-8 mx-auto">
+    <div class="sm:flex flex-wrap w-full mx-auto">
+    <?php if ( is_front_page() ): 
+      echo do_shortcode( '[contact-form-7 id="109" title="Site Contact" html_id="contact-form-1234" html_class="flex flex-wrap pt-8"]' );
+      ; endif; 
+    ?>
       @php dynamic_sidebar('sidebar-footer') @endphp
     </div>
   </div>
