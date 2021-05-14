dea2af6d189f0950553adeb326d54b7f25985497
Green-Zero-Development <admin@greenzero.us>
2021-05-14 16:19:46 -0400

    update home page picture

--- resources/views/service-page.blade.php
+++ resources/views/service-page.blade.php
@@ -9,17 +9,16 @@
 
     <style>
     .<?php echo sanitize_title_with_dashes(get_the_title(get_the_ID())); ?>-hero {
-    background-image: url("<?php the_post_thumbnail_url() ?>");
-    background-size: cover;
+    background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='28' height='49' viewBox='0 0 28 49'%3E%3Cg fill-rule='evenodd'%3E%3Cg id='hexagons' fill='%230070c0' fill-opacity='0.04' fill-rule='nonzero'%3E%3Cpath d='M13.99 9.25l13 7.5v15l-13 7.5L1 31.75v-15l12.99-7.5zM3 17.9v12.7l10.99 6.34 11-6.35V17.9l-11-6.34L3 17.9zM0 15l12.98-7.5V0h-2v6.35L0 12.69v2.3zm0 18.5L12.98 41v8h-2v-6.85L0 35.81v-2.3zM15 0v7.5L27.99 15H28v-2.31h-.01L17 6.35V0h-2zm0 49v-8l12.99-7.5H28v2.31h-.01L17 42.15V49h-2z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E");
+    background-size: 10px;
     background-attachment: fixed;
-    background-repeat: no-repeat;
     }
     .<?php echo sanitize_title_with_dashes(get_the_title(get_the_ID())); ?>-square {
       display: none;
     }
     </style>
 
-    <div class="flex flex-wrap -mt-20 z-0 <?php echo sanitize_title_with_dashes(get_the_title(get_the_ID())); ?>-hero" data-aos="fade" data-aos-duration="1000">
+    <div class="flex flex-wrap -mt-24 z-0 <?php echo sanitize_title_with_dashes(get_the_title(get_the_ID())); ?>-hero" data-aos="fade" data-aos-duration="1000">
       <div class="w-5/6 lg:w-2/3 pt-48 pb-32 lg:pt-48 lg:pb-32 text-white mx-auto">
         <h1 class="font-open text-3xl md:text-4xl lg:text-7xl font-thin pb-5 leading-none" data-aos="fade-left" data-aos-duration="1200" data-aos-easing="ease-in-sine"><?php the_title() ?></h1>
         <p class="font-open font-thin text-base lg:text-xl" data-aos="fade-left" data-aos-duration="1500" data-aos-easing="ease-in-sine"><?php the_content() ?></p>
