3ef57520ae8affcc5248dcb451383fbf77f501ec
Green-Zero-Development <admin@greenzero.us>
2021-05-14 16:30:04 -0400

    update home page picture

--- resources/views/partials/inner-page-services.blade.php
+++ resources/views/partials/inner-page-services.blade.php
@@ -10,39 +10,39 @@
         </a>
         
     </div>
-    <a href="/billing-revenue-cycle-review/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 border-color-85c879 hover:border-4 service-square billingrevenue-cycle-review-square">
+    <a href="/billing-revenue-cycle-review/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 service-square billingrevenue-cycle-review-square">
     <div class="py-24"></div>
     <h2 class="absolute bottom-0 left-0 font-oswald text-3xl font-bold text-white p-4 text-left">Billing/Revenue Cycle Review</h2>
     </a>
-    <a href="/provider-enrollment/" class="relative w-full md:w-1/3 bg-gray-300 text-center py-5 bg-cover bg-center border-0 border-color-85c879 hover:border-4 service-square provider-enrollment-square">
+    <a href="/provider-enrollment/" class="relative w-full md:w-1/3 bg-gray-300 text-center py-5 bg-cover bg-center border-0 service-square provider-enrollment-square">
     <div class="py-24"></div>
     <h2 class="absolute bottom-0 left-0 font-oswald text-3xl font-bold text-white p-4 text-left">Provider Enrollment</h2>
     </a>
-    <a href="/managed-care-insurance-contracting-assistance/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 border-color-85c879 hover:border-4 service-square managed-care-insurance-contracting-assistance-square">
+    <a href="/managed-care-insurance-contracting-assistance/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 service-square managed-care-insurance-contracting-assistance-square">
     <div class="py-24"></div>
     <h2 class="absolute bottom-0 left-0 font-oswald text-3xl font-bold text-white p-4 text-left">Managed Care &amp; Insurance Contracting Assistance</h2>
     </a>
-    <a href="/healthcare-analytics-and-special-projects/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 border-color-85c879 hover:border-4 service-square healthcare-analytics-special-projects-square">
+    <a href="/healthcare-analytics-and-special-projects/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 service-square healthcare-analytics-special-projects-square">
     <div class="py-24"></div>
     <h2 class="absolute bottom-0 left-0 font-oswald text-3xl font-bold text-white p-4 text-left">Healthcare Analytics and Special Projects</h2>
     </a>
-    <a href="/em-leveling-review/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 border-color-85c879 hover:border-4 service-square em-leveling-review-square">
+    <a href="/em-leveling-review/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 service-square em-leveling-review-square">
     <div class="py-24"></div>
     <h2 class="absolute bottom-0 left-0 font-oswald text-3xl font-bold text-white p-4 text-left">Evaluation &amp; Management Leveling Review</h2>
     </a>
-    <a href="/coding-and-chart-review/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 border-color-85c879 hover:border-4 service-square coding-and-chart-review-square">
+    <a href="/coding-and-chart-review/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 service-square coding-and-chart-review-square">
     <div class="py-24"></div>
     <h2 class="absolute bottom-0 left-0 font-oswald text-3xl font-bold text-white p-4 text-left">Coding and Chart Review</h2>
     </a>
-    <a href="/business-office-assessment/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 border-color-85c879 hover:border-4 service-square business-office-assessment-square">
+    <a href="/business-office-assessment/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 service-square business-office-assessment-square">
     <div class="py-24"></div>
     <h2 class="absolute bottom-0 left-0 font-oswald text-3xl font-bold text-white p-4 text-left">Business Office Assessment</h2>
     </a>
-    <a href="/denial-review-and-management/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 border-color-85c879 hover:border-4 service-square denial-review-and-management-square">
+    <a href="/denial-review-and-management/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 service-square denial-review-and-management-square">
     <div class="py-24"></div>
     <h2 class="absolute bottom-0 left-0 font-oswald text-3xl font-bold text-white p-4 text-left">Denial Review and Management</h2>
     </a>
-    <a href="/insurance-and-accounts-receivable-recovery/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 border-color-85c879 hover:border-4 service-square insurance-and-accounts-receivable-recovery-square">
+    <a href="/insurance-and-accounts-receivable-recovery/" class="relative w-full md:w-1/3 bg-gray-200 text-center py-5 bg-cover bg-center border-0 service-square insurance-and-accounts-receivable-recovery-square">
     <div class="py-24"></div>
     <h2 class="absolute bottom-0 left-0 font-oswald text-3xl font-bold text-white p-4 text-left">Insurance and Accounts Recievable Recovery</h2>
     </a>
